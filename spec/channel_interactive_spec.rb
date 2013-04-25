275cac89da54a9a5d610aa664a5fcd72e9d617b6
Caleb Crane <github@simulacre.org>
2013-04-25 09:19:24 +0900

    refs #20 pending spec for interactive module

---
+++ spec/channel_interactive_spec.rb
@@ -0,0 +1,10 @@
+require 'em-ssh'
+require 'em-ssh/connection/channel/interactive'
+require_relative "spec_helper"
+
+module EM::Ssh::Test
+  include Constants
+  describe EM::Ssh::Connection::Channel::Interactive do
+    pending
+  end
+end
